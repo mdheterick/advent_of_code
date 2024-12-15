00f33b399dd9e79246ab3e91b63c4d1963f10b5d
Michael Heterick <michael.heterick92@gmail.com>
2024-12-15 17:37:30 +1000

    Day 15

--- 2024/15/program2.ts
+++ 2024/15/program2.ts
@@ -36,7 +36,7 @@ const example2 = `########
 
 <^^>>>vv<v>>v<<`;
 
-const input = example;
+const input = data;
 
 const dirMap: Record<string, [number, number]> = {
     "<": [-1, 0],
@@ -50,13 +50,16 @@ class Robot extends Coordinate {
         super(x, y);
     }
 
-    get symbol() {
+    symbol(x: number, y: number) {
         return "@";
     }
 
-    runMove(move: string, grid: Array<Coordinate>) {
+    runMove(move: string, grid: Array<Robot | Wall | Box>) {
         const dir = dirMap[move];
-        const existing = grid.find(e => e.matches(this.x + dir[0], this.y + dir[1]));
+        const existing = grid.find(e => e.matches(
+            this.x + dir[0],
+            this.y + dir[1]
+        ));
         if (!existing) {
             this.move(dir);
         }
@@ -78,42 +81,71 @@ class Robot extends Coordinate {
 }
 
 class Wall extends Coordinate {
-    get symbol() {
+    symbol(x: number, y: number) {
         return "#";
+    }
+
+    matches(x: number, y: number) {
+        return this.y === y && (this.x === x || this.x + 1 === x);
     }
 }
 
-class Box extends Coordinate {
-    get symbol() {
-        return "O";
+class Box {
+    right: Coordinate;
+    left: Coordinate;
+    constructor(x: number, y: number) {
+        this.left = new Coordinate(x, y);
+        this.right = new Coordinate(x + 1, y);
+    }
+    symbol(x: number, y: number) {
+        if (this.left.x === x) return "[";
+        return "]";
     }
 
-    canMove(dir: [number, number], grid: Array<Coordinate>): boolean {
-        const next = grid.find(e => e.matches(this.x + dir[0], this.y + dir[1]));
-        if (!next) return true;
-        if (next instanceof Box) return next.canMove(dir, grid);
-        if (next instanceof Robot) throw new Error("WTF");
-        return false;
-    }
-    
-    move(dir: [number, number], grid: Array<Coordinate>) {
-        const next = grid.find(e => e.matches(this.x + dir[0], this.y + dir[1]));
-        if (next instanceof Box) {
-            next.move(dir, grid);
-        }
-        this.x = this.x + dir[0];
-        this.y = this.y + dir[1];
+    matches(x: number, y: number) {
+        return this.left.matches(x, y) || this.right.matches(x, y);
     }
 
     gps() {
-        return this.y * 100 + this.x;
+        return this.left.y * 100 + this.left.x;
+    }
+
+    canMove(dir: [number, number], grid: Array<Robot | Wall | Box>): boolean {
+        let next = this.findNext(dir, grid);
+        if (next.length === 0) return true;
+        if (next.every((e): e is Box => e instanceof Box)) return next.every(b => b.canMove(dir, grid));
+        if (next instanceof Robot) throw new Error("WTF");
+        return false;
+    }
+
+    findNext(dir: [number, number], grid: Array<Robot | Wall | Box>) {
+        let next = [
+            ...grid.filter(e => e.matches(this.left.x + dir[0], this.left.y + dir[1])),
+            ...grid.filter(e => e.matches(this.right.x + dir[0], this.right.y + dir[1]))
+        ];
+        next = next.filter(e => e !== this);
+        const nextSet = new Set(next);
+        return [...nextSet];
+    }
+    
+    move(dir: [number, number], grid: Array<Robot | Wall | Box>) {
+        let next = this.findNext(dir, grid);
+        next.forEach(n => {
+            if (n instanceof Box) {
+                n.move(dir, grid);
+            }
+        });
+        this.left.x = this.left.x + dir[0];
+        this.right.x = this.right.x + dir[0];
+        this.left.y = this.left.y + dir[1];
+        this.right.y = this.right.y + dir[1];
     }
 }
 
 function buildEntities(x: number, y: number, col: string) {
-    if (col === "@") return new Robot(x, y);
-    if (col === "#") return new Wall(x, y);
-    if (col === "O") return new Box(x, y);
+    if (col === "@") return new Robot(x * 2, y);
+    if (col === "#") return new Wall(x * 2, y);
+    if (col === "O") return new Box(x * 2, y);
     return null;
 }
 
@@ -122,25 +154,27 @@ function printGrid(maxX: number, maxY: number, entities: Array<Robot | Wall | Bo
     for (const y of range(maxY)) {
         const row: Array<string> = [];
         for (const x of range(maxX)) {
-            const matches = entities.find(e => e.matches(x, y));
-            row.push(matches ? matches.symbol : ".");
+            const matches = entities.find(e => e.matches(x , y));
+            row.push(matches ? matches.symbol(x, y) : ".");
         }
         grid.push(row.join(""));
     }
-    console.log(grid.join("\n"));
+    return grid.join("\n");
 }
 
 const [gridRaw, moves] = input.split("\n\n");
 
 const entities = gridRaw.split("\n").map((row, y) => row.split("").map((col, x) => buildEntities(x, y, col))).flat().filter((e): e is Wall | Box | Robot => e !== null);
+const robot: Robot = entities.find((e): e is Robot => e instanceof Robot)!;
 
-const robot: Robot = entities.find((e): e is Robot => e.symbol === "@")!;
+const maxX = Math.max(...entities.map(e =>  e instanceof Wall ? e.x : 0));
+const maxY = Math.max(...entities.map(e => e instanceof Wall ? e.y : 0));
 
-const maxX = Math.max(...entities.map(e => e.x));
-const maxY = Math.max(...entities.map(e => e.y));
-
+let count = 0;
 for (const move of moves.split("")) {
     if (move === "\n") continue;
     robot.runMove(move, entities);
+    count++;
 }
-console.log("Part 1", sum(entities.filter((e): e is Box => e instanceof Box).map(b => b.gps())));
+// console.log(printGrid(maxX + 2, maxY + 1, entities));
+console.log("Part 2", sum(entities.filter((e): e is Box => e instanceof Box).map(b => b.gps())));
