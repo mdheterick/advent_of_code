a10901d2a78fe7f4038bc688525ef58cc5699221
Michael Heterick <michael.heterick92@gmail.com>
2024-12-19 18:25:37 +1000

    Day 19
    Got a bit stuck with not implementing memoisation on the DFS

--- 2024/19/program.ts
+++ 2024/19/program.ts
@@ -1,3 +1,45 @@
 import { fetchInput } from "../../fetchInput";
+import { memoise, sum } from "../../utils";
 
 const data = await fetchInput(2024, 19);
+
+const example = `r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`;
+
+const input = data;
+
+
+const [_towels, _designs] = input.split("\n\n");
+
+const towels = _towels.split(", ");
+
+function isPossible(design: string) {
+    const applicableTowels = towels.filter(t => design.startsWith(t));
+    if (design.length === 0) {
+        return 1;
+    }
+    let sum = 0;
+    for (const towel of applicableTowels) {
+        const next = design.replace(towel, "");
+        sum += memoised(next);
+    }
+    return sum;
+}
+
+const memoised = memoise(isPossible);
+
+const designs = _designs.split("\n");
+
+const combinations = designs.map(d => {
+    return memoised(d);
+});
+console.log("Part 1", sum(combinations.map(c => c > 0)));
+console.log("Part 2", sum(combinations));
--- utils.ts
+++ utils.ts
@@ -87,6 +87,7 @@ export class Coordinate {
     }
 }
 
+// Always consider memoising when doing a DFS to ensure that the tail of each input only needs to be calculated once and the result can be reused
 export function memoise(func: (...args: any[]) => any) {
     const memo = new Map();
     return function(...args: any[]) {
